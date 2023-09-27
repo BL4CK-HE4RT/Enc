cdcb2281633b6d88353fb78091328a1d635c960a
IND SAGAR CYBER KING <131788623+SAGAR-CYBER99@users.noreply.github.com>
2023-09-27 08:49:56 +0530

    sagarenc.py

--- sagarenc.py
+++ sagarenc.py
@@ -3,7 +3,7 @@ import platform,os,sys
 bit = platform.architecture()[0]
 if bit == '64bit':
     print(" [///] You Are 64 BIT USER")
-    import encrypt_enc
+    import encrypt_enc.py
 
 elif bit == '32bit':
     print(" [///] sorry bit is not frequent")
